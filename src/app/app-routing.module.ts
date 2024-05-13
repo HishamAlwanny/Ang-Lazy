ea7f7ad11d61d4daa3617498bff68e2b5500dec3
Hisham Alwanni <hisham.alwanny@hotmail.com>
2024-05-13 18:37:31 +0300

    Home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NoPageFoundComponentComponent } from './no-page-found-component/no-page-found-component.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+    {path: '', component: HomeComponent},
     { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) },
     { path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule) },
     { path: 'crud', loadChildren: () => import('./crud/crud.module').then(m => m.CrudModule) },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoPageFoundComponentComponent } from './no-page-found-component/no-page-found-component.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NoPageFoundComponentComponent
+    NoPageFoundComponentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
