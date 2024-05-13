c267ff71b59020cfa2ac3c162e3cfdca01a85b9c
Hisham Alwanni <hisham.alwanny@hotmail.com>
2024-05-13 17:18:50 +0300

    Add no page found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NoPageFoundComponentComponent } from './no-page-found-component/no-page-found-component.component';
 
 const routes: Routes = [
     { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) },
-    { path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule) }];
+    { path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule) },
+
+    { path: '**', component: NoPageFoundComponentComponent },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoPageFoundComponentComponent } from './no-page-found-component/no-page-found-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoPageFoundComponentComponent
   ],
   imports: [
     BrowserModule,
